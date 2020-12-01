06a56c337e669e90a66ce2a138c8a3e181412fb8
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-01 10:58:47 +0330

    Implementation webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,37 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// Panel Styles
+mix.styles(
+    [
+        'resources/css/animate.css',
+        'resources/css/bootstrap.min.css',
+        'resources/css/chart.min.css',
+        'resources/css/fontawesome.css',
+        'resources/css/select2.min.css',
+        'resources/css/bootstrap-datepicker.min.css',
+        'resources/css/persianDatepicker-default.css',
+        'resources/css/datatable.min.css',
+
+    ]
+    , 'public/bundle/panel.css');
+
+
+// Panel Scripts
+mix.scripts(
+    [
+        'resources/js/jquery.min.js',
+        'resources/js/jquery.form.min.js',
+        'resources/js/bootstrap.min.js',
+        'resources/js/chart.min.js',
+        'resources/js/progressbar.min.js',
+        'resources/js/datatables.min.js',
+        'resources/js/select2.full.min.js',
+        'resources/js/sweetalert.min.js',
+    ]
+    , 'public/bundle/panel.js');
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
