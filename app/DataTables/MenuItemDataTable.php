9368ca7678fd409131ffbe43aa4b02ee58a33dde
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-07 14:43:31 +0330

    add columns to MenuItemDataTable

--- app/DataTables/MenuItemDataTable.php
+++ app/DataTables/MenuItemDataTable.php
@@ -21,7 +21,23 @@ class MenuItemDataTable extends BaseDataTable implements DatabaseInterface
     {
         return datatables()
             ->eloquent($query)
-            ->addColumn('action', 'menuitem.action');
+            ->addIndexColumn()
+            ->editColumn('parent_name', function ($row){
+                return isset($row->parent_id) ? $row->parent->name : '-';
+            })
+            ->filterColumn('parent_name' , function ($query, $keyword) {
+                return $query->where('name', 'like', '%'.$keyword.'%');
+            })
+            ->editColumn('action', function ($row){
+                return sprintf('
+                    <div class="dropdown">
+                        <button class="btn btn-primary btn-sm" type="button" data-toggle="dropdown">عملیات <i class="fa fa-chevron-down"></i> </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="%s" title="">ویرایش</a></li>
+                        </ul>
+                    </div>
+                ',  route('panel.menu_items.edit', ['menu_item' => $row->id]));
+            });
     }
 
     /**
@@ -32,7 +48,7 @@ class MenuItemDataTable extends BaseDataTable implements DatabaseInterface
      */
     public function query(MenuItem $model)
     {
-        return $model->newQuery()->orderBy('id');
+        return $model->newQuery()->orderBy('parent_id');
     }
 
     /**
@@ -43,20 +59,12 @@ class MenuItemDataTable extends BaseDataTable implements DatabaseInterface
     public function getColumns()
     {
         return [
-            Column::make('id'),
-            Column::make('name'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+            Column::make('DT_RowIndex', 'DT_RowIndex')->title('#')->width(20)->searchable(false),
+            Column::make('name')->title(trans('validation.attributes.name'))->orderable()->orderDataType('dom-text'),
+            Column::make('parent_name', 'name')->title(trans('validation.attributes.parent_name')),
+            Column::make('jalali_created_at')->title(trans('validation.attributes.created_at'))->name('menu_items.created_at')->searchable(false),
+            Column::make('jalali_updated_at')->title(trans('validation.attributes.updated_at'))->name('menu_items.created_at')->searchable(false),
+            Column::computed('action')->title(trans('validation.attributes.action'))->orderable(false)->exportable(false),
         ];
-    }
-
-    /**
-     * Get filename for export.
-     *
-     * @return string
-     */
-    protected function filename()
-    {
-        return 'MenuItem_' . date('YmdHis');
     }
 }
