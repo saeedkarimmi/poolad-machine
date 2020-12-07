a7e8bc2610ebd6bf33699d53582e23ad318ede3d
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-07 14:52:15 +0330

    create dashboard controller and view

---
+++ resources/views/panel/dashboard/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.panel')
+
+@section('page-title')
+    {{ trans('dashboard.title') }}
+@endsection
+
+@section('content')
+   <h1>DASHBOARD</h1>
+@endsection
+
+@push('scripts')
+    <script>
+    </script>
+@endpush
