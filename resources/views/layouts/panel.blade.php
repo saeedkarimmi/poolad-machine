07cf45d2b120b175d85c33c3df175f9e946ffaa5
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-08 11:49:24 +0330

    Initial implementation view!

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,36 +4,61 @@
     @include('partials.panel.head')
 </head>
 <body>
-<div class="container-fluid">
-    <nav class="site-header sticky-top py-1">
-        <div class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-            </a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+<div class="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand" href="#">پولاد ماشین</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @foreach ($menuItems as $menuItem)
+                <ul class="navbar-nav ">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ $menuItem->name }}
+                        </a>
+                        @if (isset($menuItem->children) and count($menuItem->children))
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @foreach ($menuItem->children as $item)
+                                <a class="dropdown-item" href="#">{{ $item->name }}</a>
+                            @endforeach
+                        </div>
+                        @endif
+                    </li>
+                </ul>
+            @endforeach
+
         </div>
     </nav>
+    <main>
+        <div class="container main-content">
+            <div class="row">
+                <div class="col-12">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </main>
 </div>
 
-<div class="container">
+<footer class="container py-5">
     <div class="row">
-        <div class="col-12">
-            @yield('content')
+        <div class="col-12 col-md">
+            <h5>footer</h5>
+            <small class="d-block mb-3 text-muted">© 2017-2018</small>
         </div>
     </div>
-</div>
+</footer>
+
 @include('partials.panel.scripts')
 <script>
     $(document).ready(function () {
-        @if ($errors->any())
-            var errors = {!! json_encode($errors->all(), true) !!};
-            console.log(errors);
+            @if ($errors->any())
+        var errors = {!! json_encode($errors->all(), true) !!};
+        console.log(errors);
         @endif
     });
 </script>
