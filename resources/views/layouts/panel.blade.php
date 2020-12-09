0b50f3571d52deda952a44e6557e8c42f2e2efe0
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-09 11:35:11 +0330

    styling!

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -13,26 +13,72 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            @foreach ($menuItems as $menuItem)
-                <ul class="navbar-nav ">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{ $menuItem->name }}
+
+{{--            @foreach ($menuItems as $menuItem)--}}
+{{--                <ul class="navbar-nav ">--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"--}}
+{{--                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                            {{ $menuItem->name }}--}}
+{{--                        </a>--}}
+{{--                        @if (isset($menuItem->children) and count($menuItem->children))--}}
+{{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">--}}
+{{--                            @foreach ($menuItem->children as $item)--}}
+{{--                                <a class="dropdown-item" href="#">{{ $item->name }}</a>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                        @endif--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+            <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            <span>test 1</span>
                         </a>
-                        @if (isset($menuItem->children) and count($menuItem->children))
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @foreach ($menuItem->children as $item)
-                                <a class="dropdown-item" href="#">{{ $item->name }}</a>
-                            @endforeach
-                        </div>
-                        @endif
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 2
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 3
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 4
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 5
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 6
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="">
+                            <img src="{{ asset('images/menu-test.png') }}" alt="">
+                            test 7
+                        </a>
                     </li>
                 </ul>
-            @endforeach
+{{--            @endforeach--}}
 
         </div>
     </nav>
+
     <main>
         <div class="container main-content">
             <div class="row">
@@ -42,16 +88,16 @@
             </div>
         </div>
     </main>
-</div>
 
-<footer class="container py-5">
-    <div class="row">
-        <div class="col-12 col-md">
-            <h5>footer</h5>
-            <small class="d-block mb-3 text-muted">© 2017-2018</small>
+    <footer class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md">
+                <h5>footer</h5>
+                <small class="d-block mb-3 text-muted">© 2017-2018</small>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</div>
 
 @include('partials.panel.scripts')
 <script>
