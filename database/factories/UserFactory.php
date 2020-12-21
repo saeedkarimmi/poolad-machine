39845f177b6cacc50f260798f9ee137b073cb7c8
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-21 09:58:06 +0330

    add last_name and active fields to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
+            'active' => rand(0,1),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
