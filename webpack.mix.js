0652fe52980d98485a35342afb0c080d5a5f26a9
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-22 15:48:17 +0330

    update webpack due to cms package

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,10 +25,13 @@ mix.styles(
     [
         'vendor/saeedkarimmi/cms/src/assets/css/bootstrap.min.css',
         'vendor/saeedkarimmi/cms/src/assets/css/font-awesome.min.css',
+        'vendor/saeedkarimmi/cms/src/assets/css/plugins/iCheck/custom.css',
         'vendor/saeedkarimmi/cms/src/assets/css/plugins/toastr/toastr.min.css',
         'vendor/saeedkarimmi/cms/src/assets/css/plugins/dataTables/datatables.min.css',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/gritter/jquery.gritter.css',
         'vendor/saeedkarimmi/cms/src/assets/css/animate.css',
+        'vendor/saeedkarimmi/cms/src/assets/css/plugins/select2/select2.min.css',
+        'vendor/saeedkarimmi/cms/src/assets/css/plugins/select2/select2-bootstrap4.min.css',
         'vendor/saeedkarimmi/cms/src/assets/css/style.css',
 
     ]
@@ -52,6 +55,8 @@ mix.scripts(
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/peity/jquery.peity.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/demo/peity-demo.js',
         'vendor/saeedkarimmi/cms/src/assets/js/inspinia.js',
+        'vendor/saeedkarimmi/cms/src/assets/js/icheck.min.js',
+        'vendor/saeedkarimmi/cms/src/assets/js/jquery.form.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/pace/pace.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/jquery-ui/jquery-ui.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/gritter/jquery.gritter.min.js',
@@ -60,6 +65,7 @@ mix.scripts(
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/sparkline/jquery.sparkline.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/demo/sparkline-demo.js',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/chartJs/Chart.min.js',
+        'vendor/saeedkarimmi/cms/src/assets/js/plugins/select2/select2.full.min.js',
         'vendor/saeedkarimmi/cms/src/assets/js/plugins/toastr/toastr.min.js',
     ]
     , 'public/bundle/panel.js');
