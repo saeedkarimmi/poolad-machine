15f34ba295221e40dc684aa277dd922ba2da62f5
Saeed Karimi <saeedkarimi454@gmail.com>
2020-12-30 17:42:38 +0330

    add default traits

--- app/Models/Panel/SystemControl.php
+++ app/Models/Panel/SystemControl.php
@@ -2,13 +2,16 @@
 
 namespace App\Models\Panel;
 
+use App\Traits\PersianDateTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wildside\Userstamps\Userstamps;
 
 class SystemControl extends Model
 {
-    use HasFactory;
-    public $timestamps = false;
+    use HasFactory,  Userstamps, SoftDeletes, PersianDateTrait;
+
     protected $table = 'tbl_system_controls';
     protected $guarded = ['id'];
 }
