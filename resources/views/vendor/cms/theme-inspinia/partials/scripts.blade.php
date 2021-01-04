f9ac58b9252f20dd86cf303643ee3a709d6a802e
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-04 14:49:03 +0330

    remove additional codes and change script stack location

--- resources/views/vendor/cms/theme-inspinia/partials/scripts.blade.php
+++ resources/views/vendor/cms/theme-inspinia/partials/scripts.blade.php
@@ -1,51 +1,6 @@
 @stack('prepend-scripts')
 <script src="{{ asset('bundle/panel.js') }}"></script>
-<!-- Mainly scripts -->
-{{--<script src="{{ asset('js/jquery.min.js') }}"></script>--}}
-{{--<script src="{{ asset('js/popper.min.js') }}"></script>--}}
-{{--<script src="{{ asset('js/bootstrap.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>--}}
-
-<!-- Flot -->
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.symbol.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/flot/jquery.flot.time.js') }}"></script>--}}
-
-
-<!-- Peity -->
-{{--<script src="{{ asset('js/plugins/peity/jquery.peity.min.js') }}"></script>--}}
-{{--<script src="{{ asset('js/demo/peity-demo.js') }}"></script>--}}
-
-<!-- Custom and plugin javascript -->
-{{--<script src="{{ asset('js/inspinia.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>--}}
-
-<!-- jQuery UI -->
-{{--<script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>--}}
-
-<!-- GITTER -->
-{{--<script src="{{ asset('js/plugins/gritter/jquery.gritter.min.js') }}"></script>--}}
-
-{{--<script src="{{ asset('js/plugins/dataTables/datatables.min.js') }}"></script>--}}
-{{--<script src="{{ asset('js/plugins/dataTables/dataTables.bootstrap4.min.js') }}"></script>--}}
-
-<!-- Sparkline -->
-{{--<script src="{{ asset('js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>--}}
-
-<!-- Sparkline demo data  -->
-{{--<script src="{{ asset('js/demo/sparkline-demo.js') }}"></script>--}}
-
-<!-- ChartJS-->
-{{--<script src="{{ asset('js/plugins/chartJs/Chart.min.js') }}"></script>--}}
-
-<!-- Toastr -->
-{{--<script src="{{ asset('js/plugins/toastr/toastr.min.js') }}"></script>--}}
-
+@stack('scripts')
 <script src="{{ asset('js/panel.js') }}"></script>
 
 
@@ -57,5 +12,3 @@
         @endif
     });
 </script>
-
-@stack('scripts')
