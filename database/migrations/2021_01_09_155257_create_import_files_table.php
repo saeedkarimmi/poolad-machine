bdbd171c2656080f7128eed181ed721dec49f061
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-16 09:00:03 +0330

    change date columns type to string in order to persian date

--- database/migrations/2021_01_09_155257_create_import_files_table.php
+++ database/migrations/2021_01_09_155257_create_import_files_table.php
@@ -19,22 +19,24 @@ class CreateImportFilesTable extends Migration
             $table->unsignedBigInteger('order_id');
             $table->foreign('order_id')->references('id')->on('tbl_orders');
 
+            $table->unsignedTinyInteger('currency_id');
+            $table->foreign('currency_id')->references('id')->on('tbl_currencies');
+
+            $table->unsignedTinyInteger('bank_id');
+            $table->foreign('bank_id')->references('id')->on('tbl_banks');
+
             $table->string('file_number', 50)->nullable();
             $table->string('order_registration_code', 50)->nullable();
             $table->string('proforma_number', 50)->nullable();
             $table->string('incoterm', 50)->nullable();
 
-            $table->timestamp('order_register_completion_date')->nullable();
-            $table->timestamp('order_register_issue_date')->nullable();
-            $table->timestamp('order_register_validity_date')->nullable();
+            $table->string('order_register_completion_date', 20)->nullable();
+            $table->string('order_register_issue_date', 20)->nullable();
+            $table->string('order_register_validity_date', 20)->nullable();
 
-            $table->timestamp('currency_allocate_application_date')->nullable();
-            $table->timestamp('currency_allocate_issue_date')->nullable();
-            $table->timestamp('validity_currency_allotment_date')->nullable();
-
-//            currency_id
-//            bank_id
-
+            $table->string('currency_allocate_application_date', 20)->nullable();
+            $table->string('currency_allocate_issue_date', 20)->nullable();
+            $table->string('validity_currency_allotment_date', 20)->nullable();
 
             $table->timestamps();
             $table->unsignedBigInteger('created_by')->nullable();
