2177b398552644c767c6f799780b973c596f33d4
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-24 11:49:11 +0330

    add file_number and remove currency_id condition

--- database/migrations/2021_01_23_173958_create_v_transfer_files_view.php
+++ database/migrations/2021_01_23_173958_create_v_transfer_files_view.php
@@ -25,14 +25,14 @@ class CreateVTransferFilesView extends Migration
                         tbl_banks.`name` AS bank_name,
                         tbl_banks.id AS bank_id,
                         tbl_sellers.id AS seller_id,
-                        tbl_sellers.`name` AS seller_name
+                        tbl_sellers.`name` AS seller_name,
+	                    tbl_import_files.file_number
                     FROM
                         tbl_transfer_files
                         INNER JOIN tbl_import_files ON tbl_transfer_files.import_file_id = tbl_import_files.id
                         INNER JOIN tbl_orders ON tbl_import_files.order_id = tbl_orders.id
                         INNER JOIN tbl_sellers ON tbl_orders.seller_id = tbl_sellers.id
                         INNER JOIN tbl_currencies ON tbl_import_files.currency_id = tbl_currencies.id
-                        AND tbl_orders.currency_id = tbl_currencies.id
                         INNER JOIN tbl_payment_methods ON tbl_orders.payment_method_id = tbl_payment_methods.id
                         INNER JOIN tbl_banks ON tbl_import_files.bank_id = tbl_banks.id"
         );
